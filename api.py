20ee23572d2597cbf3a2af17a81e44bf9df49472
EthanHargrove <ethanhargrove99@gmail.com>
2024-02-28 22:51:15 +0000

    New puzzle generation

--- api.py
+++ api.py
@@ -1,35 +1,40 @@
 import numpy as np
 import polars as pl
 
+
 def generate_sudoku():
-    df = pl.read_csv("sudoku.csv", dtypes={'quizzes': str})
-    row_index = np.random.randint(low=0, high=1_000_000)
-    puzzle = np.array(list(df["quizzes"][row_index])).reshape((9,9)).astype(int)
+    df = pl.read_parquet("sudoku.parquet")
+    row_index = np.random.randint(low=0, high=284_905)
+    puzzle = np.array(list(df["puzzle"][row_index])).reshape((9, 9)).astype(int)
     return puzzle
 
+
 def update_notes(puzzle, notes):
-    puzzle = np.array(puzzle, dtype=int).reshape((9,9))
-    notes = np.array(notes, dtype=int).reshape((9,9,9))
+    puzzle = np.array(puzzle, dtype=int).reshape((9, 9))
+    notes = np.array(notes, dtype=int).reshape((9, 9, 9))
     new_notes = notes.copy()
 
     for i, row in enumerate(puzzle):
         for j, num in enumerate(row):
             if 0 < num < 10:
                 # Remove row notes
-                new_notes[i, :, num-1] = 0
+                new_notes[i, :, num - 1] = 0
                 # Remove column notes
-                new_notes[:, j, num-1] = 0
+                new_notes[:, j, num - 1] = 0
                 # Remove box notes
                 start_row = 3 * (i // 3)
                 start_col = 3 * (j // 3)
-                new_notes[start_row:start_row+3, start_col:start_col+3, num-1] = 0
-    
+                new_notes[
+                    start_row : start_row + 3, start_col : start_col + 3, num - 1
+                ] = 0
+
     changes = notes - new_notes
-    
+
     num_changes = np.sum(changes)
     print(num_changes)
 
     return new_notes, changes, num_changes
+
 
 def naked_singles(puzzle, notes):
     """
@@ -47,9 +52,9 @@ def naked_singles(puzzle, notes):
         changes : 9x9x9 np array of boolean integers
             there
     """
-    new_puzzle = np.array(puzzle, dtype=int).reshape((9,9))
-    notes = np.array(notes, dtype=int).reshape((9,9,9))
-    changes = np.zeros((9,9,9), dtype=int)
+    new_puzzle = np.array(puzzle, dtype=int).reshape((9, 9))
+    notes = np.array(notes, dtype=int).reshape((9, 9, 9))
+    changes = np.zeros((9, 9, 9), dtype=int)
     num_changes = 0
 
     try:
@@ -64,5 +69,5 @@ def naked_singles(puzzle, notes):
                 num_changes += 1
     except:
         pass
-    
+
     return new_puzzle, changes, num_changes
