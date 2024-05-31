9bba4ead071883019725d0c712259d3a7fa814b4
EthanHargrove <ethanhargrove99@gmail.com>
2024-05-31 20:52:58 +0100

    Completed intro to ML

--- frontend/src/components/Q-Learning/IntroToML.tsx
+++ frontend/src/components/Q-Learning/IntroToML.tsx
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from "react";
-// import { useSpring, animated } from "react-spring";
-// import { Link } from "react-router-dom";
-import { Grid, Stack } from "@mui/material";
-import {
-  Stage,
-  Layer,
-  Circle,
-  Line,
-  Arrow,
-  Text,
-  Rect,
-  Image,
-} from "react-konva";
-import useImage from "use-image";
+import { display, fontStyle } from "@mui/system";
+import { info } from "console";
 import { title } from "process";
-// import { styled } from "@mui/system";
+import React, { useState, useEffect } from "react";
+import { Stage, Layer, Circle, Arrow, Text, Rect, Image } from "react-konva";
+import useImage from "use-image";
 
-function IntroToRL() {
+interface IntroToMLProps {
+  defaultParadigm: string;
+}
+function IntroToML({ defaultParadigm }: IntroToMLProps) {
+  const [currentParadigm, setCurrentParadigm] = useState(defaultParadigm);
   const [dimensions, setDimensions] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -51,20 +44,39 @@ function IntroToRL() {
   const [image2] = useImage("./images/unsupervised_learning.png");
   const [image3] = useImage("./images/reinforcement_learning.png");
 
-  const circleRadius = Math.max(dimensions.width * 0.06, 45); // 5% of the smaller window dimension
-  const circleY = rectY + rectHeight + circleRadius * 1.75;
-  const circleSpacing = rectWidth / 8;
+  const circleRadius = Math.max(
+    Math.min(dimensions.width * 0.055, dimensions.height * 0.085),
+    45
+  );
+  const circleY =
+    dimensions.width > 444
+      ? rectY + rectHeight + circleRadius * 1.9
+      : rectY + rectHeight + dimensions.height * 0.115;
+  const circleSpacing = dimensions.width > 444 ? rectWidth / 8 : 20;
   const circles = [
-    { x: rectX + circleSpacing, image: image1, title: "Supervised\nLearning" },
+    {
+      x: rectX + circleSpacing,
+      image: image1,
+      title: "Supervised\nLearning",
+      displayInfo: currentParadigm === "Supervised\nLearning",
+      infoText:
+        "• Trained on labelled data\n\n\n• Model learns the relationship between inputs and outputs by identifying patterns in the training data\n\n\n• Once trained, the model can predict outputs for new, unseen inputs",
+    },
     {
       x: rectX + rectWidth / 2,
       image: image2,
       title: "Unsupervised\nLearning",
+      displayInfo: currentParadigm === "Unsupervised\nLearning",
+      infoText:
+        "• Training on unlabelled data\n\n\n• Model aims to identify patterns, structures, or relationships within the data\n\n\n• Often used for clustering, dimensionality reduction, and anomaly detection",
     },
     {
       x: rectX + rectWidth - circleSpacing,
       image: image3,
       title: "Reinforcement\nLearning",
+      displayInfo: currentParadigm === "Reinforcement\nLearning",
+      infoText:
+        "• Learning from experience, through trial and error\n\n\n• An agent interacts with an environemnt, receives rewards based on actions taken, and uses those rewards to inform future decisions\n\n\n• Often used to solve complex control problems",
     },
   ];
 
@@ -77,8 +89,12 @@ function IntroToRL() {
     ];
   };
 
+  const titleFontSize = circleRadius * 0.28;
   const titleY = circleY + circleRadius * 1.1;
-  const titleFontSize = circleRadius * 0.25;
+  const infoY =
+    dimensions.width > 444
+      ? titleY + 1.7 * circleRadius
+      : titleY + 2.25 * circleRadius;
 
   return (
     <div className="section" style={{ background: "#ffffff" }}>
@@ -107,6 +123,16 @@ function IntroToRL() {
             verticalAlign="middle"
           />
           {circles.map((circle, index) => {
+            let shadowBlur;
+            let titleFontStyle;
+            let shadowColour = "black";
+            if (circle.displayInfo) {
+              shadowBlur = 25;
+              titleFontStyle = "500";
+              shadowColour = "#b85959";
+            } else {
+              shadowBlur = 5;
+            }
             const controlPoints = getControlPoints(
               rectX + rectWidth / 2,
               rectY + rectHeight,
@@ -115,12 +141,36 @@ function IntroToRL() {
             );
             return (
               <React.Fragment key={index}>
+                {/* info background */}
+                <Rect
+                  x={rectX + circleSpacing - circleRadius - 10}
+                  y={infoY - circleRadius * 0.725}
+                  width={rectWidth - 2 * (circleSpacing - circleRadius - 10)}
+                  height={circleRadius * 4}
+                  cornerRadius={[
+                    circle.title === "Supervised\nLearning" ? 0 : circleRadius,
+                    circle.title === "Reinforcement\nLearning"
+                      ? 0
+                      : circleRadius,
+                    circleRadius,
+                    circleRadius,
+                  ]}
+                  fill={circle.displayInfo ? "#b85959" : ""}
+                />
+                {/* image background */}
                 <Circle
                   x={circle.x}
                   y={circleY}
-                  radius={circleRadius}
-                  fill="white"
-                  shadowBlur={5}
+                  radius={circleRadius + 10}
+                  fill={circle.displayInfo ? "#b85959" : ""}
+                />
+                {/* background connector */}
+                <Rect
+                  x={circle.x - circleRadius - 10}
+                  y={circleY}
+                  width={2 * circleRadius + 20}
+                  height={circleRadius * 4}
+                  fill={circle.displayInfo ? "#b85959" : ""}
                 />
                 <Image
                   x={circle.x - circleRadius}
@@ -129,19 +179,33 @@ function IntroToRL() {
                   height={circleRadius * 2}
                   image={circle.image}
                   cornerRadius={circleRadius}
+                  shadowColor={shadowColour}
+                  shadowBlur={shadowBlur}
+                  listening
+                  onClick={() => setCurrentParadigm(circle.title)}
+                  onTouchStart={() => setCurrentParadigm(circle.title)}
+                  onMouseEnter={(e) => {
+                    const container = e.target.getStage()!.container();
+                    container.style.cursor = "pointer";
+                  }}
+                  onMouseLeave={(e) => {
+                    const container = e.target.getStage()!.container();
+                    container.style.cursor = "default";
+                  }}
                 />
                 <Arrow
                   points={[
                     rectX + rectWidth / 2,
-                    rectY + rectHeight, // Start point
+                    rectY + rectHeight,
                     controlPoints[0].x,
-                    controlPoints[0].y, // First control point
+                    controlPoints[0].y,
                     controlPoints[1].x,
-                    controlPoints[1].y, // Second control point
+                    controlPoints[1].y,
                     circle.x,
                     circleY -
                       circleRadius -
-                      Math.min(stageHeight, stageWidth) * 0.015, // End point
+                      5 -
+                      Math.min(stageHeight, stageWidth) * 0.015,
                   ]}
                   pointerLength={10}
                   pointerWidth={10}
@@ -150,47 +214,47 @@ function IntroToRL() {
                   strokeWidth={2}
                   tension={0.5}
                 />
+                {/* title */}
                 <Text
-                  x={circle.x - circleRadius}
+                  x={circle.x - circleRadius - 10}
                   y={titleY}
                   text={circle.title}
-                  width={circleRadius * 2}
+                  width={2 * circleRadius + 18}
                   fontSize={titleFontSize}
                   fontFamily="SpaceGrotesk"
-                  fontStyle="500"
+                  fontStyle={titleFontStyle}
                   wrap="word"
-                  fill="black"
+                  fill={currentParadigm === circle.title ? "white" : "black"}
                   align="center"
                   verticalAlign="middle"
+                  onClick={() => setCurrentParadigm(circle.title)}
+                  onTouchStart={() => setCurrentParadigm(circle.title)}
+                  onMouseEnter={(e) => {
+                    const container = e.target.getStage()!.container();
+                    container.style.cursor = "pointer";
+                  }}
+                  onMouseLeave={(e) => {
+                    const container = e.target.getStage()!.container();
+                    container.style.cursor = "default";
+                  }}
+                />
+                {/* info text */}
+                <Text
+                  x={rectX + circleSpacing - circleRadius + 5}
+                  y={infoY}
+                  text={circle.displayInfo ? circle.infoText : ""}
+                  width={
+                    rectWidth - 2 * (circleSpacing - circleRadius - 10) - 20
+                  }
+                  fontSize={titleFontSize}
+                  fontFamily="SpaceGrotesk"
+                  wrap="word"
+                  fill="white"
                   listening={false}
                 />
               </React.Fragment>
             );
           })}
-          {/* <Text
-            x={rectX + circleSpacing}
-            y={titleY}
-            text={"Supervised\nLearning"}
-            fontSize={titleFontSize}
-            fontFamily="SpaceGrotesk"
-            wrap="word"
-            fill="black"
-            align="center"
-            verticalAlign="middle"
-            listening={false}
-          />
-          <Text
-            x={rectX - circleRadius * 2.5}
-            y={titleY}
-            text={"Unsupervised\nLearning"}
-            fontSize={titleFontSize}
-            fontFamily="SpaceGrotesk"
-            wrap="word"
-            fill="black"
-            align="center"
-            verticalAlign="middle"
-            listening={false}
-          /> */}
         </Layer>
       </Stage>
       {/* <h1>Machine Learning</h1>
@@ -208,7 +272,7 @@ function IntroToRL() {
   );
 }
 
-export default IntroToRL;
+export default IntroToML;
 
 // const IntroToRL = () => {
 //   return (
@@ -253,44 +317,3 @@ export default IntroToRL;
 //     </div>
 //   );
 // };
-
-// const styles = {
-//   container: {
-//     fontFamily: "Arial, sans-serif",
-//     padding: "20px",
-//     maxWidth: "800px",
-//     margin: "0 auto",
-//     backgroundColor: "#f4f4f4",
-//     borderRadius: "10px",
-//     boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-//   },
-//   title: {
-//     color: "#333",
-//   },
-//   section: {
-//     marginBottom: "20px",
-//   },
-//   subtitle: {
-//     color: "#666",
-//     fontSize: "20px",
-//     borderBottom: "1px solid #ddd",
-//     paddingBottom: "5px",
-//     marginBottom: "10px",
-//   },
-//   text: {
-//     color: "#333",
-//     fontSize: "16px",
-//     lineHeight: "1.6",
-//   },
-//   list: {
-//     listStyleType: "none",
-//     padding: "0",
-//   },
-//   listItem: {
-//     color: "#333",
-//     fontSize: "16px",
-//     marginBottom: "5px",
-//   },
-// };
-
-// export default IntroToRL;
--- frontend/src/pages/QLearning.tsx
+++ frontend/src/pages/QLearning.tsx
@@ -1,31 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
 
 import Navbar from "../components/Navbar";
-import IntroToRL from "../components/Q-Learning/IntroToRL";
+import IntroToML from "../components/Q-Learning/IntroToML";
 import "../styles/gnn.css";
 
 const QLearning = () => {
   useEffect(() => {
     document.title = "Ethan Hargrove - Q-Learning";
   }, []);
+
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Navbar active="" />
       <ReactFullpage
         credits={{ enabled: false, label: "" }}
-        navigation
+        navigation={dimensions.width > 444}
+        scrollingSpeed={1000}
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
-              {/* Your full-page sections go here */}
               <div className="section" style={{ background: "#ffffff" }}>
                 <h1 className="heading">
                   Reinforcement Learning:
                   <br /> Q-Learning
                 </h1>
               </div>
-              <IntroToRL />
+              <IntroToML defaultParadigm={"Reinforcement\nLearning"} />
               <div className="section" style={{ background: "#ab4f6e" }}>
                 Section 3
               </div>
